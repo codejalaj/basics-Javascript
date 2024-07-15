f96247b15b17effe779f098b2564cae211134078
codejalaj <143094254+codejalaj@users.noreply.github.com>
2024-07-15 21:22:39 +0000

    conversionOperation

---
+++ 01_basics/3_conversionOperation.js
@@ -0,0 +1,14 @@
+// let num = 33
+
+// let stringNumber = String(num)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+let str1 = "hy"
+let str2 = " world"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 + "2");
