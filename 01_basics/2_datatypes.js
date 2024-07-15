ebaeed11917c596b61f3fb6c1802b0adb1dbcd79
codejalaj <143094254+codejalaj@users.noreply.github.com>
2024-07-15 18:35:04 +0000

    datatypes

---
+++ 01_basics/2_datatypes.js
@@ -0,0 +1,18 @@
+" use strict"; // treat all JS code as newer version
+
+
+let name = "jalaj" // string datatypes
+let age = 20  // number type
+let isLoggedIn = true //boolean type
+
+
+
+// number range is => 2 to pwr 53
+ //bigint use for big numbers ex 132423423423
+ // string => ""
+ // boolean => true/false
+ // null =  object or standalone value
+ // symbol => unique 
+
+
+ 
